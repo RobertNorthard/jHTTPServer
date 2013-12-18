2d1827a5299ed4e2a148fda256850613356746f6
RobertNorthard <robertnorthard@googlemail.com>
2013-12-18 20:30:38 +0000

    Added rediret to index.html

--- src/main/java/HTTPResponse.java
+++ src/main/java/HTTPResponse.java
@@ -8,45 +8,42 @@ import java.io.*;
  * @version 18/12/2013
  */
 public class HTTPResponse
-{
-    private HTTPRequest request = null;
+{		
     private static final String HTTP_VERSION = "HTTP/1.1";
     private List<String> headers = new ArrayList<String>();
     private byte[] content = null;
-
-    public HTTPResponse(HTTPRequest request){
-        this.request = request;
-    }
     
     /**
      * Handle HTTP response
      */
-    public void handleRequest()
-                    throws IOException{
-        
-        String requestedResource = this.request.getResource();
-        switch(this.request.getRequestMethod()){
+    public void handleRequest(HTTPRequest request) throws IOException{
+
+      	String resourcePath = "www/" + request.getResource();
+
+		switch(request.getRequestMethod()){
             
             case HEAD: 
                 this.setHeaders(ResponseCode._200);
                 break;
             case GET: 
-                File file = new File("www/" + requestedResource);
-                if(file.exists() && !requestedResource.equals("/")){
-                    this.setHeaders(ResponseCode._200);
-                    this.getFile(file);
-                }else{
-                    this.setHeaders(ResponseCode._404);
-                }
+                File file = new File(resourcePath);
+				//direct to index.html if resource /
+				if(request.getResource().equals("/")){
+					file = new File("www/index.html");
+					this.setHeaders(ResponseCode._200);
+					this.setContent(file); 
+               } else if (file.exists()){
+					this.setHeaders(ResponseCode._200);
+                    this.setContent(file);
+				}else
+					this.setHeaders(ResponseCode._404);
         } 
     }
-    
+
     /**
      * Reads the contents of a file
      */
-    public void getFile(File file)
-                                throws IOException{
-                                    
+    public void setContent(File file) throws IOException{                             
         InputStream in = new FileInputStream(file);
         this.content = new byte[(int)file.length()];
         int offSet = 0;
@@ -55,31 +52,32 @@ public class HTTPResponse
             int rc = in.read(this.content, offSet, (content.length - offSet));
             offSet += rc;
         }
-        
         in.close(); 
-    }
-    
-    /**
-     * Write the HTTP headers and file if applicable to client
-     */
-    public void write(OutputStream out)
-                                throws IOException{
-        for(String header : this.headers)
-            out.write(header.getBytes());
-        
-        if(this.content != null)
-            out.write(this.content);
     }
     
     /**
      * Set the HTTP Headers
      */
     public void setHeaders(ResponseCode code){
-        
          headers.add(this.HTTP_VERSION  + " " + code.toString() + "\r\n");
                 headers.add("Date: " + new Date().toString()+ "\r\n");
                 headers.add("Server: Robert's Webserver"+ "\r\n");
-                headers.add("Connection: close"+ "\r\n\r\n");
-        
+                headers.add("Connection: close"+ "\r\n\r\n");  
     } 
+
+	/**
+	* Return HTTP Response Headers
+	* @return HTTP Response headers
+	*/
+	public List<String> getHeaders(){
+		return this.headers;
+	}
+	
+	/**
+	* Return HTTP Response content
+	* @return HTTP response content
+	*/
+	public byte[] getContent(){
+		return this.content;
+	}
 }
