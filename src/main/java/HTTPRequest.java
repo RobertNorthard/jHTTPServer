2eaa66476889f6d3ec901c3b1b33221cbe2c1beb
RobertNorthard <robertnorthard@googlemail.com>
2013-12-18 20:30:02 +0000

    Refactored - Class Cohesion

--- src/main/java/HTTPRequest.java
+++ src/main/java/HTTPRequest.java
@@ -2,70 +2,89 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+/**
+ * A class to handle HTTP requests. Only takes into account first line.
+ * 
+ * @author Robert Northard
+ * @version 18/12/2013
+ */
 public class HTTPRequest{
 
-    private InputStream in = null;
-    List<String> requestLines = null;
-    MethodCode method = null;
-    String resource = "";
+    private MethodCode method = null;
+    private String resource = "";
+	private String version = "";
 
     /**
      * Constructor for class HTTPRequest
-     * @param in InputStream to read request
+     * @param method http request type
+	 * @param resource requested resource name
+	 * @param version http version
      */
-    public HTTPRequest(InputStream in){
-
-        this.in = in;
-        this.requestLines = new ArrayList<String>();
-
+    public HTTPRequest(MethodCode method, String resource, String version){
+        this.method = method;
+		this.resource = resource;
+		this.version = version;
     }
 
     /**
-     * get Request
+     * Parse first HTTP request line
+     * Example: GET / HTTP/1.1
+	 * Break it down into request type, requested resource and HTTP version.
+	 * pre-condition: valid http request line
+	 * @return a HTTP Request
      */
-    public void getRequest(){
+    public static HTTPRequest parseRequest(String requestLine){
+        String[] parts = requestLine.split(" ");
+		
+		MethodCode method = null;
+		String resource = null;
+		String version = null;
+		
+		if(parts.length == 3){
+		
+			//not the best approach
+        	try{
+            	method = MethodCode.valueOf(parts[0]);
+        	}catch (IllegalArgumentException e){
+            	method = MethodCode.UNRECOGNIZED;
+        	}
 
-        Scanner scan = new Scanner(this.in);
-        int nLine = 0; //number of lines read
-
-        while(true){
-
-            String line = scan.nextLine();
-            if(line.length() == 0) break; //end of header lines
-            this.requestLines.add(line);
-
-        }
-
-        this.parseRequest();
+        	resource = parts[1];
+			version = parts[2];
+		}
+		
+		return new HTTPRequest(method, resource, version);
     }
+
+	/**
+	* set resouce file name
+	* @param resource the resource file name
+	*/
+	public void setResource(String resource){
+		this.resource = resource;
+	}
 
     /**
      * Return requested resource
+	 * @return resouce file name 
      */
     public String getResource(){
         return this.resource;
     }
 
+	/**
+	* Return HTTP version
+	* @return HTTP version
+	*/
+	public String getVersion(){
+		return this.version;
+	}
+
     /**
-     * Return request method 
+     * Return request method type 
+	 * @return HTTP method request type
      */
     public MethodCode getRequestMethod(){
         return this.method;
     }
-
-    /**
-     * Parse request 
-     */
-    private void parseRequest(){
-
-        Scanner scan = new Scanner(this.requestLines.get(0));
-        try{
-
-            this.method = MethodCode.valueOf(scan.next());
-        }catch (IllegalArgumentException e){
-            this.method = MethodCode.UNRECOGNIZED;
-        }
-        this.resource = scan.next();
-    }
-
 }
