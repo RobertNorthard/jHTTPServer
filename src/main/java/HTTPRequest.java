16e8459f799408d558b8a9875ea2ec79175fe8fa
RobertNorthard <robertnorthard@googlemail.com>
2013-12-19 15:41:34 +0000

    Indents

--- src/main/java/HTTPRequest.java
+++ src/main/java/HTTPRequest.java
@@ -4,95 +4,95 @@ import java.net.*;
 import java.util.*;
 
 /**
-* A class that represents a HTTP request.
-*
-* @author Robert Northard
-* @version 18/12/2013
-*/
+ * A class that represents a HTTP request.
+ *
+ * @author Robert Northard
+ * @version 18/12/2013
+ */
 public class HTTPRequest{
-    
+
     private MethodCode method = null;
     private String resource = "";
     private String version = "";
-    
+
     /**
-    * Constructor for class HTTPRequest
-    * @param method http request type
-    * @param resource requested resource name
-    * @param version http version
-    */
+     * Constructor for class HTTPRequest
+     * @param method http request type
+     * @param resource requested resource name
+     * @param version http version
+     */
     public HTTPRequest(MethodCode method, String resource, String version){
         this.method = method;
         this.resource = resource;
         this.version = version;
     }
-    
+
     /**
-    * Parse first HTTP request line
-    * Example: GET / HTTP/1.1
-    * Break it down into request type, requested resource and HTTP version.
-    * pre-condition: valid http request line
-    * @return a HTTP Request
-    */
+     * Parse first HTTP request line
+     * Example: GET / HTTP/1.1
+     * Break it down into request type, requested resource and HTTP version.
+     * pre-condition: valid http request line
+     * @return a HTTP Request
+     */
     public static HTTPRequest parseRequest(String requestLine){
         String[] parts = requestLine.split(" ");
-        
+
         MethodCode method = null;
         String resource = null;
         String version = null;
-        
+
         if(parts.length == 3){
-            
+
             //not the best approach
             try{
                 method = MethodCode.valueOf(parts[0]);
-                }catch (IllegalArgumentException e){
+            }catch (IllegalArgumentException e){
                 method = MethodCode.UNRECOGNIZED;
             }
-            
+
             resource = parts[1];
             version = parts[2];
         }
-        
+
         return new HTTPRequest(method, resource, version);
     }
-    
+
     /**
-    * Read a request, only need first line.
-    */
+     * Read a request, only need first line.
+     */
     public static String recieveRequest(InputStream in) throws IOException{
         Scanner scan = new Scanner(in);
         return scan.nextLine();
     }
-    
+
     /**
-    * set resouce file name
-    * @param resource the resource file name
-    */
+     * set resouce file name
+     * @param resource the resource file name
+     */
     public void setResource(String resource){
         this.resource = resource;
     }
-    
+
     /**
-    * Return requested resource
-    * @return resource file name
-    */
+     * Return requested resource
+     * @return resource file name
+     */
     public String getResource(){
         return this.resource;
     }
-    
+
     /**
-    * Return HTTP version
-    * @return HTTP version
-    */
+     * Return HTTP version
+     * @return HTTP version
+     */
     public String getVersion(){
         return this.version;
     }
-    
+
     /**
-    * Return request method type
-    * @return HTTP method request type
-    */
+     * Return request method type
+     * @return HTTP method request type
+     */
     public MethodCode getRequestMethod(){
         return this.method;
     }
--- src/main/java/HTTPResponse.java
+++ src/main/java/HTTPResponse.java
@@ -8,99 +8,99 @@ import java.io.*;
  * @version 18/12/2013
  */
 public class HTTPResponse
-{		
+{       
     private static final String HTTP_VERSION = "HTTP/1.1";
     private List<String> headers = new ArrayList<String>();
     private byte[] content = null;
-	private HTTPRequest request = null;
+    private HTTPRequest request = null;
 
-	/**
-	* Constructor for class HTTP response
-	* @param request HTTP request to generate a HTTP response for
-	*/
+    /**
+     * Constructor for class HTTP response
+     * @param request HTTP request to generate a HTTP response for
+     */
     public HTTPResponse(HTTPRequest request) throws IOException{
-		this.request = request; 
-	}
+        this.request = request; 
+    }
 
     /** TODO: other response codes
-    * Handle HTTP response
- 	* @param request HTTP request to generate a HTTP response for
-    */
+     * Handle HTTP response
+     * @param request HTTP request to generate a HTTP response for
+     */
     public void handleRequest() throws IOException{
 
-      	String resourcePath = "www/" + request.getResource();
+        String resourcePath = "www/" + request.getResource();
 
-		switch(request.getRequestMethod()){
-            
+        switch(request.getRequestMethod()){
+
             case HEAD: 
-                this.setHeaders(ResponseCode._200);
-                break;
+            this.setHeaders(ResponseCode._200);
+            break;
             case GET: 
-                File file = new File(resourcePath);
-                //direct to index.html if resource /
-                if(request.getResource().equals("/")){
-				    file = new File("www/index.html");
-					this.setHeaders(ResponseCode._200);
-					this.setContent(file); 
-                }else if (file.exists()){
-					this.setHeaders(ResponseCode._200);
-                    this.setContent(file);
-                }else
-                    this.setHeaders(ResponseCode._404);
+            File file = new File(resourcePath);
+            //direct to index.html if resource /
+            if(request.getResource().equals("/")){
+                file = new File("www/index.html");
+                this.setHeaders(ResponseCode._200);
+                this.setContent(file); 
+            }else if (file.exists()){
+                this.setHeaders(ResponseCode._200);
+                this.setContent(file);
+            }else
+                this.setHeaders(ResponseCode._404);
         } 
     }
 
-	/**
-    * Write the HTTP headers and file if applicable to client
-    */
+    /**
+     * Write the HTTP headers and file if applicable to client
+     */
     public void writeResponse(OutputStream out) throws IOException{
-		//write headers
+        //write headers
         for(String header : headers)
             out.write(header.getBytes());
-        
-		//write content
+
+        //write content
         if(this.content != null)
-           out.write(this.content);
+            out.write(this.content);
     }
 
     /**
-    * Reads the contents of a file
-    */
+     * Reads the contents of a file
+     */
     public void setContent(File file) throws IOException{                             
         InputStream in = new FileInputStream(file);
         this.content = new byte[(int)file.length()];
         int offSet = 0;
         while(offSet < file.length()){
-            
+
             int rc = in.read(this.content, offSet, (content.length - offSet));
             offSet += rc;
         }
         in.close(); 
     }
-    
+
     /**
-    * Set the HTTP Headers
-    */
+     * Set the HTTP Headers
+     */
     public void setHeaders(ResponseCode code){
-         headers.add(this.HTTP_VERSION  + " " + code.toString() + "\r\n");
-                headers.add("Date: " + new Date().toString()+ "\r\n");
-                headers.add("Server: Robert's Webserver"+ "\r\n");
-                headers.add("Connection: close"+ "\r\n\r\n");  
+        headers.add(this.HTTP_VERSION  + " " + code.toString() + "\r\n");
+        headers.add("Date: " + new Date().toString()+ "\r\n");
+        headers.add("Server: Robert's Webserver"+ "\r\n");
+        headers.add("Connection: close"+ "\r\n\r\n");  
     } 
 
-	/**
-	* Return HTTP Response Headers
-	* @return HTTP Response headers
-	*/
-	public List<String> getHeaders(){
-		return this.headers;
-	}
-	
-	/**
-	* Return HTTP Response content
-	* @return HTTP response content
-	*/
-	public byte[] getContent(){
-		return this.content;
-	}
+    /**
+     * Return HTTP Response Headers
+     * @return HTTP Response headers
+     */
+    public List<String> getHeaders(){
+        return this.headers;
+    }
+
+    /**
+     * Return HTTP Response content
+     * @return HTTP response content
+     */
+    public byte[] getContent(){
+        return this.content;
+    }
 }
