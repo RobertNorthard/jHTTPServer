6266c3976ec38e3475d490898a19c0085dd72fb0
RobertNorthard <robertnorthard@googlemail.com>
2013-12-19 11:15:04 +0000

    Format

--- src/main/java/HTTPServer.java
+++ src/main/java/HTTPServer.java
@@ -3,48 +3,48 @@ import java.net.*;
 import java.util.*;
 
 /**
- * A basic concurrent HTTP Web server
- * 
- * @author Robert Northard
- * @version 01/09/2012
- */
+* A basic concurrent HTTP Web server
+* 
+* @author Robert Northard
+* @version 01/09/2012
+*/
 public class HTTPServer
 {
-    private static final int DEFAULT_PORT = 8000;
+   private static final int DEFAULT_PORT = 8000;
 
-    private String directory = "";
-    private ServerSocket serverSocket = null;
+   private String directory = "";
+   private ServerSocket serverSocket = null;
 
-    /**
-     * Constructor for class HTTP Web Server
-     * @param name name of server
-     * @param port port listening number
-     * @trows IOException
-     */
-    public HTTPServer(int port) throws IOException{
-        if(!isValidPort(port)) port = DEFAULT_PORT;
-        this.serverSocket = new ServerSocket(port);
-    }
+   /**
+    * Constructor for class HTTP Web Server
+    * @param name name of server
+    * @param port port listening number
+    * @trows IOException
+    */
+   public HTTPServer(int port) throws IOException{
+       if(!isValidPort(port)) port = DEFAULT_PORT;
+       this.serverSocket = new ServerSocket(port);
+   }
 
-    /**
-     * Return true if valid port number, 0 - 65535 inclusive.
-     * @return true if valid port number
-     */
-    public static boolean isValidPort(int port){      
-        return (port > 0 && port < 65535);
-    }
+   /**
+    * Return true if valid port number, 0 - 65535 inclusive.
+    * @return true if valid port number
+    */
+   public static boolean isValidPort(int port){      
+       return (port > 0 && port < 65535);
+   }
 
-    /**
-     * Invoke to start
-     */
-    public void run() throws IOException{
+   /**
+    * Invoke to start
+    */
+   public void run() throws IOException{
 
-        System.out.println("Server listening on: " + this.serverSocket.getInetAddress().getHostAddress() + ":" + this.serverSocket.getLocalPort());
+       System.out.println("Server listening on: " + this.serverSocket.getInetAddress().getHostAddress() + ":" + this.serverSocket.getLocalPort());
 
-        while(true){
-            Socket conn = this.serverSocket.accept();
-            System.out.println("Accepted Connection: " + conn.getRemoteSocketAddress() + " " + new Date());
-            new RequestHandler(conn).start();
-        }    
-    }
+       while(true){
+           Socket conn = this.serverSocket.accept();
+           System.out.println("Accepted Connection: " + conn.getRemoteSocketAddress() + " " + new Date());
+           new RequestHandler(conn).start();
+       }    
+   }
 }
